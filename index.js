26ddcb45a81b242bc49cd0110229d46362c20804
ASIEDU RICHMOND <richieasiedu19@gmail.com>
2025-03-22 06:30:49 +0000

    temp convert

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-let prev = document.querySelector('.prev')
-let next = document.querySelector('.next')
-
-prev.addEventListener('click', function(){
-    let items = document.querySelectorAll('.item')
-    document.querySelector('.slide').prepend(items[items.length - 1])
-})
-
-next.addEventListener('click', function(){
-    let items = document.querySelectorAll('.item')
-    document.querySelector('.slide').appendChild(items[0])
-})
+const textBox = document.getElementById("textBox")
+const toFahrenheit = document.getElementById("toFahrenheit");
+const toCelsius = document.getElementById("toCelsius");
+const result = document.getElementById("result");
+let temp;
+function convert(){
+    if(toFahrenheit.checked){
+        temp = Number(textBox.value);
+        temp = temp * 9 / 5 + 32;
+        result.textContent = temp.toFixed(1) + "°F"
+    }
+    else if(toCelsius.checked){
+        temp = Number(textBox.value);
+        temp = (temp - 32) * (5/9);
+        result.textContent = temp.toFixed(1) + "°C"
+    }
+    else{
+        result.textContent = "Select a Unit"
+    }
+}
