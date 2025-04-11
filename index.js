7e71f45a1359073ed2960d7240cd2e485c60a64a
ASIEDU RICHMOND <richieasiedu19@gmail.com>
2025-04-11 22:53:05 +0000

    toggle

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-const containerEl = document.querySelector(".container");
-const careers = ["YouTuber", "Web Designer", "Freelancer", "Instructor"];
-let careerIndex = 0;
-let characterIndex = 0;
-updateText()
-function updateText(){
-    characterIndex++
-    containerEl.innerHTML = `
-    <h1>I am ${careers[careerIndex].slice(0,1) === "I" ? "an" : "a"} ${careers[careerIndex ].slice(0,characterIndex)}</h1>
-    `;
-    
-    if(characterIndex === careers[careerIndex].length){
-        careerIndex++
-        characterIndex = 0;
+const inputEl = document.querySelector(".input");
+const bodyEl = document.querySelector("body");
+inputEl.checked = JSON.parse(localStorage.getItem("mode"));
+
+updateBody()
+
+function updateBody() {
+    if(inputEl.checked){
+        bodyEl.style.background = "black";
+    }else{
+        bodyEl.style.background = "white";
     }
-    if(careerIndex === careers.length){
-        careerIndex = 0;
-    }
-    setTimeout(updateText, 400)
+}
+inputEl.addEventListener("input", ()=>{
+    updateBody()
+    updateLocalStorage()
+})
+
+function updateLocalStorage(){
+    localStorage.setItem("mode", JSON.stringify(inputEl.checked))
 }
