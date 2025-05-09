0a987b1c071d475621e1646b836ba52e85741d38
ASIEDU RICHMOND <richieasiedu19@gmail.com>
2025-05-09 06:54:28 +0000

    portfo modified

--- index.js
+++ index.js
@@ -1,21 +1,75 @@
-const inputEl = document.querySelector(".input");
-const bodyEl = document.querySelector("body");
-inputEl.checked = JSON.parse(localStorage.getItem("mode"));
+let darkmode = localStorage.getItem("darkmode");
+const themeSwitchEl = document.getElementById("theme-switch");
 
-updateBody()
+const enableDarkmode = () =>{
+    document.body.classList.add("darkmode");
+    localStorage.setItem("darkmode", "active");
+};
+const disableDarkmode = () =>{
+    document.body.classList.remove("darkmode");
+    localStorage.setItem("darkmode", null);
+};
+if(darkmode === "active") enableDarkmode();
 
-function updateBody() {
-    if(inputEl.checked){
-        bodyEl.style.background = "black";
-    }else{
-        bodyEl.style.background = "white";
+themeSwitchEl.addEventListener("click", () =>{
+    darkmode = localStorage.getItem("darkmode");
+    darkmode  !== "active" ? enableDarkmode() : disableDarkmode();
+});
+
+const searchBarContainerEl = document.querySelector(".search-bar-container");
+const magnifierEl = document.querySelector(".magnifier");
+magnifierEl.addEventListener("click", () =>{
+    searchBarContainerEl.classList.toggle("active");
+});
+
+const countersEl = document.querySelectorAll(".counter");
+
+countersEl.forEach((counterEl) => {
+    counterEl.innerText = "0";
+    incrementCounter();
+
+    function incrementCounter(){
+        let currentNum = +counterEl.innerText;
+        const dataCeil = counterEl.getAttribute("data-ceil");
+        const increment = dataCeil / 8;
+        currentNum = Math.floor(currentNum + increment);
+        if(currentNum < dataCeil){
+            counterEl.innerText = currentNum;
+            setTimeout(incrementCounter, 160);
+        }else{
+            counterEl.innerText = dataCeil;
+        }
     }
-}
-inputEl.addEventListener("input", ()=>{
-    updateBody()
-    updateLocalStorage()
-})
+});
 
-function updateLocalStorage(){
-    localStorage.setItem("mode", JSON.stringify(inputEl.checked))
-}
+const playEl = document.querySelector(".play-video");
+const closeIconEl = document.querySelector(".close-icon");
+const trailerContainerEl = document.querySelector(".trailer-container");
+const videoEl = document.querySelector("video");
+
+playEl.addEventListener("click", () =>{
+    trailerContainerEl.classList.remove("active");
+});
+closeIconEl.addEventListener("click", () =>{
+    trailerContainerEl.classList.add("active");
+    videoEl.pause();
+    videoEl.currentTime = 0;
+});
+
+const spansEl = document.querySelectorAll(".progress-bar span");
+spansEl.forEach((span) => {
+    span.style.width = span.dataset.width;
+    span.innerHTML = span.dataset.width;
+});
+
+
+let prev = document.querySelector('.prev');
+ let next = document.querySelector('.next');
+prev.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item');
+    document.querySelector('.slide').prepend(items[items.length - 1]);
+});
+next.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item');
+    document.querySelector('.slide').appendChild(items[0]);
+});
